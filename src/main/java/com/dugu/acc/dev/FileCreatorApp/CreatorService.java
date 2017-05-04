514be68f10952efed4881ccf25ea8b505f5f7643
Basant kumar Hota <basahota@users.noreply.github.com>
2017-05-04 20:36:07 +0530

    uploaded create text file from Excel

---
+++ src/main/java/com/dugu/acc/dev/FileCreatorApp/CreatorService.java
@@ -0,0 +1,53 @@
+package com.dugu.acc.dev.FileCreatorApp;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class CreatorService {
+
+	@SuppressWarnings("resource")
+	public List<String> getDirectory(String xlxFileLocation) throws IOException {
+		List<String> paths = new ArrayList<String>();
+		XSSFWorkbook workBook = new XSSFWorkbook(xlxFileLocation);
+		XSSFSheet sheet = workBook.getSheetAt(0);
+		int totalRows = sheet.getPhysicalNumberOfRows();
+		System.out.println("Total Specified path find :" + (totalRows - 1));
+		Row row;
+		for (int i = 1; i <= sheet.getLastRowNum(); i++) {
+			row = (Row) sheet.getRow(i);
+			String path = row.getCell(1).getStringCellValue();
+			paths.add(path);
+		}
+		return paths;
+	}
+
+	public void createFile(List<String> paths) {
+		for (String path : paths) {
+			try {
+				File file = new File(path);
+				if (file.createNewFile()) {
+					System.out.println("File is created! with path : " + "("
+							+ path + ")");
+				} else {
+					System.out.println("File already exists.");
+				}
+
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public static void main(String[] args) throws IOException {
+		CreatorService service = new CreatorService();
+		service.createFile(service
+				.getDirectory("C:/Users/basanta.kumar.hota/Desktop/info.xlsx"));
+
+	}
+}
